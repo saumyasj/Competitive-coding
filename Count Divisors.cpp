1b2e117411e40cbfbcf5a5bddb3d277bf7948a8b
saumz <45761512+saumz@users.noreply.github.com>
2019-05-28 19:17:37 +0530

    Create Count Divisors.cpp

---
+++ Count Divisors.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int l,r,k,i,j,c=0;
+   cin>>l>>r>>k;
+   for(i=l;i<=r;i++)
+   {
+       if(i%k==0)
+        c++;
+
+   }
+   cout<<c;
+}
